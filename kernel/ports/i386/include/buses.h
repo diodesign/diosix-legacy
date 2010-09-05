068949f1db59e7724a57eefc48b2161ea5cfd9d8
Chris Williams <chris@diodesign.co.uk>
2010-09-05 23:40:37 +0000

    added buses.h for definitions of bus management code

---
+++ kernel/ports/i386/include/buses.h
@@ -0,0 +1,26 @@
+/* kernel/ports/i386/include/buses.h
+ * prototypes and structures to describe the system's management of its hardware buses
+ * Author : Chris Williams
+ * Date   : Mon,26 Mar 2007.23:09:39
+
+Copyright (c) Chris Williams and individual contributors
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Contact: chris@diodesign.co.uk / http://www.diodesign.co.uk/
+
+*/
+
+#ifndef _BUSES_H
+#define	_BUSES_H
+
+/* prototypes */
+kresult bus_register_name(unsigned int id, char *name);
+kresult bus_add_route(unsigned int flags, unsigned int busid, unsigned int busirq, unsigned int ioapicid, unsigned int ioapicirq);
+kresult bus_find_irq(unsigned char *cpuirq, char *busname, unsigned int busirq);
+
+#endif
