ef62a35c3af129e067a35d8dab5d1a4a2f6753fe
Chris Williams <chris@diodesign.co.uk>
2011-01-23 15:02:17 +0000

    skeleton atapi driver code

---
+++ user/sbin/drivers/i386/atapi/atapi.h
@@ -0,0 +1,27 @@
+/* user/sbin/drivers/i386/atapi/atapi.h
+ * Defines for the userspace ATAPI cdrom driver
+ * Author : Chris Williams
+ * Date   : Sun,16 Jan 2011.23:21:00
+
+Copyright (c) Chris Williams and individual contributors
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Contact: chris@diodesign.co.uk / http://www.diodesign.co.uk/
+
+*/
+
+#ifndef _ATAPI_H
+#define _ATAPI_H 1
+
+/* x86-specific stuff */
+unsigned char read_port_byte(unsigned short port);
+unsigned short read_port(unsigned short port);
+void write_port_byte(unsigned short port, unsigned char val);
+void write_port(unsigned short port, unsigned short val);
+
+#endif
---
+++ user/sbin/drivers/i386/atapi/main.c
@@ -0,0 +1,130 @@
+/* user/sbin/drivers/i386/atapi/main.c
+ * Driver for ATAPI-connected storage devices 
+ * Author : Chris Williams
+ * Date   : Sun,16 Jan 2011.22:54:00
+
+Copyright (c) Chris Williams and individual contributors
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Contact: chris@diodesign.co.uk / http://www.diodesign.co.uk/
+
+*/
+
+#include "diosix.h"
+#include "functions.h"
+#include "async.h"
+#include "roles.h"
+#include "io.h"
+
+#include "atapi.h"
+#include "lowlevel.h"
+
+#include <stdio.h>
+#include <string.h>
+
+#define DEVICE_PATHNAME "/dev/atapi0"
+#define RECEIVE_BUFFER_SIZE   (2048)
+
+/* ----------------------------------------------------------------------
+                        main loop of the driver 
+   ---------------------------------------------------------------------- */
+void reply_to_request(diosix_msg_info *msg, kresult result)
+{
+   diosix_vfs_reply reply;
+   
+   /* sanity check */
+   if(!msg) return;
+   
+   /* ping a reply back to unlock the requesting thread */
+   reply.result = result;
+   msg->send = &reply;
+   msg->send_size = sizeof(diosix_vfs_reply);
+   
+   diosix_msg_reply(msg);
+}
+
+void wait_for_request(void)
+{
+   diosix_msg_info msg;
+   unsigned char recv_buffer[RECEIVE_BUFFER_SIZE];
+   
+   memset(&msg, 0, sizeof(diosix_msg_info));
+   
+   /* wait for a generic message to come in */
+   msg.flags = DIOSIX_MSG_GENERIC;
+   msg.recv = recv_buffer;
+   msg.recv_max_size = RECEIVE_BUFFER_SIZE;
+   
+   if(diosix_msg_receive(&msg) == success)
+   {
+      diosix_vfs_request_head *req_head = (diosix_vfs_request_head *)recv_buffer;
+      
+      if(msg.recv_size < sizeof(diosix_vfs_request_head))
+      {
+         /* malformed request, it's too small to even hold a request type */
+         reply_to_request(&msg, e_too_small);
+         return;
+      }
+      
+      /* sanity check the message header */
+      if(req_head->magic != VFS_MSG_MAGIC)
+      {
+         /* malformed request, bad magic */
+         reply_to_request(&msg, e_bad_magic);
+         return;
+      }
+      
+      /* decode the request type */
+      switch(req_head->type)
+      {
+         /* discard requests that aren't appropriate */
+         case link_req:
+         case readlink_req:
+         case unlink_req:
+         case symlink_req:
+         case lseek_req:
+         case register_req:
+         case deregister_req:
+            
+         /* discard requests that aren't implemented (yet) */
+         case chown_req:
+         case fstat_req:
+         case stat_req:
+            reply_to_request(&msg, e_no_handler);
+            break;
+
+         /* open device exclusively for root only */
+         case open_req:
+            /* fall through to sending back an error */
+            reply_to_request(&msg, e_no_rights);
+            break;
+
+         case close_req:
+         case read_req:
+         case write_req:
+            
+         /* if the type is unknown then fail it */
+         default:
+            reply_to_request(&msg, e_bad_params);
+      }
+   }
+}
+
+
+int main(void)
+{
+   /* move into driver layer (1) and get access to IO ports */
+   diosix_priv_layer_up(1);
+   if(diosix_driver_register()) diosix_exit(1); /* or exit on failure */
+
+   /* register this device with the vfs */
+   diosix_vfs_register(DEVICE_PATHNAME);
+   
+   /* wait for work to come in */
+   while(1) wait_for_request();
+}
