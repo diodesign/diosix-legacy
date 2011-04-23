adcd929512be4f0c2a3985c39c69f5155ed4cbe2
Chris Williams <chris@diodesign.co.uk>
2011-04-23 20:49:02 +0000

    introduced new build directory in which all binaries (executable and object files) will be placed, thus freeing the source tree of any compiler output; split build system into architecture and target so that archs can support multiple targets

--- release/i386/makeiso.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# build a bootable ISO9600 image file (suitable for CDs) called ./cd.iso out of the filesystem in ./root
-
-cp `pwd`/release/i386/stage2_eltorito `pwd`/release/i386/root/boot/grub/stage2_eltorito
-mkisofs -R -quiet -b boot/grub/stage2_eltorito -no-emul-boot -boot-load-size 4 -boot-info-table -o release/i386/cd.iso `pwd`/release/i386/root
-exit 0;
