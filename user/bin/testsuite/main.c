2a61ed21a4d806e3fc037e10c3973a0772639377
Chris Williams <chris@diodesign.co.uk>
2011-05-09 10:54:11 +0000

    begun implementing simple test system

---
+++ user/bin/testsuite/main.c
@@ -0,0 +1,32 @@
+/* user/bin/testsuite/main.c
+ * Testsuite of kernel and OS APIs
+ * Author : Chris Williams
+ * Date   : Mon,9 May 2011.11:06:00
+
+Copyright (c) Chris Williams and individual contributors
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Contact: chris@diodesign.co.uk / http://www.diodesign.co.uk/
+
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+
+#include "diosix.h"
+#include "functions.h"
+#include "async.h"
+#include "roles.h"
+
+int main(void)
+{
+   /* name this process so others can find it */
+   diosix_set_role(DIOSIX_ROLE_SYSTEM_EXECUTIVE);
+      
+   while(1); /* idle */
+}
