fa1de349b6c8d09bb70bfb95a14a34a8487504b5
Chris Williams <chris@diodesign.co.uk>
2011-07-18 23:28:41 +0000

    [rtl8139] remove noisy debug

--- user/sbin/drivers/i386/rtl8139/main.c
+++ user/sbin/drivers/i386/rtl8139/main.c
@@ -429,10 +429,7 @@ int main(void)
       phys_mem.flags = VMA_WRITEABLE | VMA_NOCACHE;
       phys_mem.paddr = nic->recv_phys[loop];
       phys_mem.vaddr = (void *)THREAD_RECV_BUFFER(nic->id, loop);
-      
-      printf("*** recv[%i] size = %x phys = %x virt = %x\n",
-             loop, (unsigned int)phys_mem.size, (unsigned int)phys_mem.paddr, (unsigned int)phys_mem.vaddr);
-      
+
       err = diosix_driver_map_phys(&phys_mem);
       if(err)
       {
@@ -465,9 +462,6 @@ int main(void)
       phys_mem.flags = VMA_WRITEABLE | VMA_NOCACHE;
       phys_mem.paddr = nic->recv_phys[loop];
       phys_mem.vaddr = (void *)THREAD_SEND_BUFFER(nic->id, loop);
-      
-      printf("*** send[%i] size = %x phys = %x virt = %x\n",
-             loop, (unsigned int)phys_mem.size, (unsigned int)phys_mem.paddr, (unsigned int)phys_mem.vaddr);
       
       err = diosix_driver_map_phys(&phys_mem);
       if(err)
